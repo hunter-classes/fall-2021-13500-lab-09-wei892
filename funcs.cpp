ccdda4ca66035c13b80085f7d0cf63c5e3253a85
Wei Lin Li <wlin.li2403@gmail.com>
2021-11-08 12:32:49 -0500

    completed task C

--- funcs.cpp
+++ funcs.cpp
@@ -5,13 +5,15 @@
 
 // add functions here
 
+//============================================Part A
 double length(Coord3D *p)
 {
   double dist = sqrt(pow(p->x , 2) + pow(p->x, 2) + pow(p->z, 2));
   return dist;
 }
 
-Coord3D *fartherFromOrgin(Coord3D *p1, Coord3D *p2)
+//============================================Part B
+Coord3D *fartherFromOrigin(Coord3D *p1, Coord3D *p2)
 {
   double distP1 = length(p1);
   double distP2 = length(p2);
@@ -28,4 +30,12 @@ Coord3D *fartherFromOrgin(Coord3D *p1, Coord3D *p2)
     {
       return p1;
     }
+}
+
+//============================================Part C
+void move(Coord3D *ppos, Coord3D *pvel, double dt)
+{
+  ppos->x += dt * (pvel->x);
+  ppos->y += dt * (pvel->y);
+  ppos->z += dt * (pvel->z);
 }
--- main.cpp
+++ main.cpp
@@ -6,12 +6,39 @@
 
 int main()
 {
+  //============================================ Part A
+  std::cout << "Part A" << std::endl;
   Coord3D pointP = {10, 20, 30};
   double x = length(&pointP);
   //std::cout << x << std::endl;
   std::cout << "Coord3D pointP = {10, 20, 30}" << std::endl;
   std::cout << "The length is: " << x << std::endl;
 
+  //============================================ Part B
+  std::cout << "\nPartB" << std::endl;
+  Coord3D pointX = {10, 20, 30};
+  std::cout << "PointX = {10, 20, 30}" << std::endl;
+  Coord3D pointQ = {-20, 21, -22};
+  std::cout << "PointQ = {-20, 21, -22}" << std::endl;
+
+  std::cout << "Address of X: " << &pointX << std::endl;
+  std::cout << "Address of Q: " << &pointQ << std::endl;
+
+  Coord3D * ans = fartherFromOrigin(&pointX, &pointQ);
+
+  std::cout << "ans = " << ans << std::endl;
+
+  //============================================ Part C
+  std::cout << "\nPart C" << std::endl;
+  Coord3D pos = {0, 0, 100.0};
+  Coord3D vel = {1, -5, 0.2};
+  std::cout << "Pos: {0, 0, 100.0}" << std::endl;
+  std::cout << "Vel: {1, -5, 0.2}" << std::endl;
+
+  move(&pos, &vel, 2.0); // object pos gets changed
+  std::cout << "The new coordinate is: " << std::endl;
+  std::cout << "{" <<pos.x << " " << pos.y << " " << pos.z << "}" << std::endl;
+  //============================================ Part D
   
   return 0;
 }
